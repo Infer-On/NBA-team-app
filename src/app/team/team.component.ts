1ceccc11ca914327ea1771f5848403f8287b82eb
Ale <a.fabozzi@live.com>
2019-06-05 07:15:24 +0200

    wip

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core'
-import { FormGroup, FormArray } from '@angular/forms'
-import { TeamFormService } from './team-form.service'
-import { Subscription } from 'rxjs'
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormArray } from '@angular/forms';
+import { TeamFormService } from './team-form.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'nba-team',
@@ -9,36 +9,36 @@ import { Subscription } from 'rxjs'
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent implements OnInit, OnDestroy {
-  teamForm: FormGroup
-  teamFormSub: Subscription
-  formInvalid: boolean = false;
-  players: FormArray
+  teamForm: FormGroup;
+  teamFormSub: Subscription;
+  formInvalid = false;
+  players: FormArray;
 
   constructor(private teamFormService: TeamFormService) { }
 
   ngOnInit() {
     this.teamFormSub = this.teamFormService.teamForm$
       .subscribe(team => {
-          this.teamForm = team
-          this.players = this.teamForm.get('players') as FormArray
-        })
+          this.teamForm = team;
+          this.players = this.teamForm.get('players') as FormArray;
+        });
   }
 
   ngOnDestroy() {
-    this.teamFormSub.unsubscribe()
+    this.teamFormSub.unsubscribe();
   }
 
   addPlayer() {
-    this.teamFormService.addPlayer()
+    this.teamFormService.addPlayer();
   }
 
   deletePlayer(index: number) {
-    this.teamFormService.deletePlayer(index)
+    this.teamFormService.deletePlayer(index);
   }
 
   saveTeam() {
-    console.log('team saved!')
-    console.log(this.teamForm.value)
+    console.log('team saved!');
+    console.log(this.teamForm.value);
   }
 }
 
